22ddbb71d8e3fcd39a1231b1927578a15c0bd6de
SadsaucR <cks6023159@gmail.com>
2025-12-26 14:20:41 +0800

    重構成MVC專案

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Password_manager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Main()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -29,6 +29,9 @@ app.UseAuthentication();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
